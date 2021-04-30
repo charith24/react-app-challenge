15f8fede697d94fa7647cd491e66a68dc9be146a
charith24 <67182579+charith24@users.noreply.github.com>
2021-05-01 00:43:15 +0530

    ch

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+var fname = "Dumpeti";
+var lname = "Charith";
+var rollno = "10";
 ReactDOM.render(
   <div>
-    <h1>My Favourite Computer Languages</h1>
-    <ol>
-      <li>PHP</li>
-      <li>JAVA</li>
-      <li>Python</li>
-      <li>javascript</li>
-    </ol>
+    <h1>Hello My first name is {fname}</h1>
+    <h1>Hello My last name is {lname}</h1>
+    <h1>
+      Hello My full name is {fname} {lname}
+    </h1>
+    <h1>Hello My full name is {fname + " " + lname}</h1>
+    <p>My roll number is {rollno}</p>
+    <p>My roll number is {rollno * 2}</p>
+    <p>My roll number is {rollno - 5}</p>
+    <p>My roll number is {rollno + 5}</p>
+    <p>My roll number is {rollno / 5}</p>
   </div>,
   document.getElementById("root")
 );
